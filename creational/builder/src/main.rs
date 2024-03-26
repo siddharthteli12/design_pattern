3f4c2a5365e447d2599f7e536c2abaf05081057a
siddharthteli12 <telisiddharth123@yahoo.in>
2024-03-26 23:46:17 +0530

    Refactor: Real builder pattern

--- creational/builder/src/main.rs
+++ creational/builder/src/main.rs
@@ -3,8 +3,10 @@ mod sandwiches;
 use sandwiches::*;
 
 fn main() {
-    let sandwich = Sandwich::init()
+    let sandwich = Sandwich::builder()
         .add_bread(vec![Bread::Garlic])
         .add_cheese(vec![Cheese::American])
         .add_meat(vec![Meat::Chicken]).build();
+
+    println!("Sandwich - {:?}", sandwich);
 }
--- creational/builder/src/sandwiches.rs
+++ creational/builder/src/sandwiches.rs
@@ -1,40 +1,106 @@
-#[derive(Clone)]
+#![allow(dead_code)]
+
+#[derive(Clone, Debug)]
 pub enum Bread {
     Garlic,
     Italic,
     Indian,
 }
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub enum Sauce {
     Chilli,
     Tomato,
     Onion,
 }
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub enum Cheese {
     American,
     Cheddar,
 }
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub enum Meat {
     Chicken,
     Turkey,
     Fish,
 }
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub enum Vegetable {
     Tomato,
     Onion,
     Corn,
 }
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub enum Condiment {
     Pepper,
     Olive,
     Honey,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
+pub struct SandwichBuilder {
+    bread: Option<Vec<Bread>>,
+    sauce: Option<Vec<Sauce>>,
+    cheese: Option<Vec<Cheese>>,
+    meat: Option<Vec<Meat>>,
+    vegetable: Option<Vec<Vegetable>>,
+    condiment: Option<Vec<Condiment>>,
+}
+
+impl SandwichBuilder {
+    pub fn new() -> Self {
+        SandwichBuilder {
+            bread: None,
+            sauce: None,
+            cheese: None,
+            meat: None,
+            vegetable: None,
+            condiment: None,
+        }
+    }
+
+    pub fn add_bread(mut self, bread: Vec<Bread>) -> Self {
+        self.bread.get_or_insert(bread);
+        self
+    }
+
+    pub fn add_sauce(mut self, sauce: Vec<Sauce>) -> Self {
+        self.sauce.get_or_insert(sauce);
+        self
+    }
+
+    pub fn add_cheese(mut self, cheese: Vec<Cheese>) -> Self {
+        self.cheese.get_or_insert(cheese);
+        self
+    }
+
+    pub fn add_meat(mut self, meat: Vec<Meat>) -> Self {
+        self.meat.get_or_insert(meat);
+        self
+    }
+
+    pub fn add_vegetables(mut self, vegetable: Vec<Vegetable>) -> Self {
+        self.vegetable.get_or_insert(vegetable);
+        self
+    }
+
+    pub fn add_condiments(mut self, condiment: Vec<Condiment>) -> Self {
+        self.condiment.get_or_insert(condiment);
+        self
+    }
+
+    pub fn build(self) -> Sandwich {
+        Sandwich {
+            bread: self.bread,
+            sauce: self.sauce,
+            cheese: self.cheese,
+            meat: self.meat,
+            vegetable: self.vegetable,
+            condiment: self.condiment,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
 pub struct Sandwich {
     bread: Option<Vec<Bread>>,
     sauce: Option<Vec<Sauce>>,
@@ -45,48 +111,7 @@ pub struct Sandwich {
 }
 
 impl Sandwich {
-    pub fn init() -> Self {
-        Sandwich {
-            bread: None,
-            sauce: None,
-            cheese: None,
-            meat: None,
-            vegetable: None,
-            condiment: None,
-        }
-    }
-
-    pub fn add_bread(&mut self, bread: Vec<Bread>) -> &mut Self {
-        self.bread.get_or_insert(bread);
-        self
-    }
-
-    pub fn add_sauce(&mut self, sauce: Vec<Sauce>) -> &mut Self {
-        self.sauce.get_or_insert(sauce);
-        self
-    }
-
-    pub fn add_cheese(&mut self, cheese: Vec<Cheese>) -> &mut Self {
-        self.cheese.get_or_insert(cheese);
-        self
-    }
-
-    pub fn add_meat(&mut self, meat: Vec<Meat>) -> &mut Self {
-        self.meat.get_or_insert(meat);
-        self
-    }
-
-    pub fn add_vegetables(&mut self, vegetable: Vec<Vegetable>) -> &mut Self {
-        self.vegetable.get_or_insert(vegetable);
-        self
-    }
-
-    pub fn add_condiments(&mut self, condiment: Vec<Condiment>) -> &mut Self {
-        self.condiment.get_or_insert(condiment);
-        self
-    }
-
-    pub fn build(&self) -> Self {
-        self.clone()
+    pub fn  builder() -> SandwichBuilder {
+        SandwichBuilder::new()
     }
 }
